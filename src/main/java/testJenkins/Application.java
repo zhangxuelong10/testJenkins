d3a9bc558da3f80a0b0a812fb8829a240ac5d91f
namexy <yuexu@viveduric.com>
2019-09-29 11:16:38 +0800

    978778

--- src/main/java/testJenkins/Application.java
+++ src/main/java/testJenkins/Application.java
@@ -10,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
 
 
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
