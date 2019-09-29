55726384c21425c6de578950972933fefe91e4e3
namexy <yuexu@viveduric.com>
2019-09-29 10:41:11 +0800

    呵呵

--- src/main/java/testJenkins/Application.java
+++ src/main/java/testJenkins/Application.java
@@ -10,6 +10,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("hello world");
         SpringApplication.run(Application.class, args
+
         );
     }
 }
